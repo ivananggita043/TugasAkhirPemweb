6316cf15602cc3800954af71e6e46db5698eca33
ivananggita043 <ivananggita043@gmail.com>
2022-01-20 14:27:55 +0700

    1

---
+++ bukutamu.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+
+    <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap"
+    rel="stylesheet"/>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@1,600&display=swap');
+        </style>
+
+    <title>biodata</title>
+</head>
+<body>
+  
+    <div>
+        <nav class="navbar">
+            <ul class="navbar-nav">
+    
+                <li class="nav-item"><!-- HOME -->
+                    <a href="index.html" class="link">
+                        <svg 
+                        aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="home" 
+                        class="svg-inline--fa fa-home fa-w-18" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 576 512">
+                            <path 
+                            fill="currentColor" 
+                            d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z">
+                            </path></svg>
+                        <span class="link-text">Home</span>
+                    </a>                
+                </li>
+    
+                <li class="nav-item"><!-- KAMERA -->
+                    <a href="kamera.html" class="link">
+                        <svg 
+                        aria-hidden="true" 
+                        focusable="false" data-prefix="fas" 
+                        data-icon="camera-retro" 
+                        class="svg-inline--fa fa-camera-retro fa-w-16" 
+                        role="img" xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 512 512">
+                            <path 
+                            fill="currentColor" 
+                            d="M48 32C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48H48zm0 32h106c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H38c-3.3 0-6-2.7-6-6V80c0-8.8 7.2-16 16-16zm426 96H38c-3.3 0-6-2.7-6-6v-36c0-3.3 2.7-6 6-6h138l30.2-45.3c1.1-1.7 3-2.7 5-2.7H464c8.8 0 16 7.2 16 16v74c0 3.3-2.7 6-6 6zM256 424c-66.2 0-120-53.8-120-120s53.8-120 120-120 120 53.8 120 120-53.8 120-120 120zm0-208c-48.5 0-88 39.5-88 88s39.5 88 88 88 88-39.5 88-88-39.5-88-88-88zm-48 104c-8.8 0-16-7.2-16-16 0-35.3 28.7-64 64-64 8.8 0 16 7.2 16 16s-7.2 16-16 16c-17.6 0-32 14.4-32 32 0 8.8-7.2 16-16 16z">
+                            </path></svg>
+                        <span class="link-text">Kamera</span>
+                    </a>
+                </li>
+    
+                <li class="nav-item"><!-- FILM -->
+                    <a href="rollfilm.html" class="link">
+                        <svg 
+                        aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="film" 
+                        class="svg-inline--fa fa-film fa-w-16" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 512 512">
+                            <path 
+                            fill="currentColor" 
+                            d="M488 64h-8v20c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12V64H96v20c0 6.6-5.4 12-12 12H44c-6.6 0-12-5.4-12-12V64h-8C10.7 64 0 74.7 0 88v336c0 13.3 10.7 24 24 24h8v-20c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v20h320v-20c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v20h8c13.3 0 24-10.7 24-24V88c0-13.3-10.7-24-24-24zM96 372c0 6.6-5.4 12-12 12H44c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40zm0-96c0 6.6-5.4 12-12 12H44c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40zm0-96c0 6.6-5.4 12-12 12H44c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40zm272 208c0 6.6-5.4 12-12 12H156c-6.6 0-12-5.4-12-12v-96c0-6.6 5.4-12 12-12h200c6.6 0 12 5.4 12 12v96zm0-168c0 6.6-5.4 12-12 12H156c-6.6 0-12-5.4-12-12v-96c0-6.6 5.4-12 12-12h200c6.6 0 12 5.4 12 12v96zm112 152c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40zm0-96c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40zm0-96c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40z">
+                            </path></svg>
+                        <span class="link-text">Film</span>
+                    </a>
+                </li>
+    
+                <li class="nav-item"><!-- PROFILE -->
+                    <a href="biodata.html" class="link">
+                        <svg 
+                        aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="user-circle" 
+                        class="svg-inline--fa fa-user-circle fa-w-16" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 496 512">
+                            <path 
+                            fill="currentColor" 
+                            d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z">
+                            </path></svg>
+                        <span class="link-text">Profile</span>
+                    </a>
+                </li>
+    
+                <li class="nav-item"><!-- BUKU TAMU -->
+                    <a href="bukutamu.html" class="link">
+                        <svg aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="bookmark" 
+                        class="svg-inline--fa fa-bookmark fa-w-12" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 384 512">
+                            <path 
+                            fill="currentColor" 
+                            d="M0 512V48C0 21.49 21.49 0 48 0h288c26.51 0 48 21.49 48 48v464L192 400 0 512z">
+                            </path></svg>
+                        <span class="link-text">Buku tamu</span>
+                    </a>
+                </li>
+    
+            </ul>
+        </nav>
+    </div>
+
+    <div class="header">
+        <header >
+            <br><h1><i>AnalogAja</i></h1>
+        </header>
+    </div>
+
+    <div class="main">
+        <br>
+        <div>
+            <form method="POST" name action="<?php echo $_SERVER['PHP_SELF'] ?>">
+                <table border="0" cellspacing="0" cellpadding="10">
+                    <h2 style="text-align: center">Login</h2>
+                    <br>
+                    <tr>
+                        <td>
+                            <label>Username: </label>                           
+                        </td>
+                        <td>
+                            <input type="text" name="nama" placeholder="Nama">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>                           
+                            <label>Email: </label>
+                        </td>
+                        <td>
+                            <input type="email" name="email" placeholder="xxx@gmail.com">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>                           
+                            <label>Paswords: </label>
+                        </td>
+                        <td>
+                            <input type="password" name="pass" placeholder="****">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Nomor telfon: </label>
+                        </td>
+                        <td>
+                            <input type="number" name="nomor" placeholder="+62">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit"></td>
+                    </tr>
+                </table>
+            </form><br>
+
+            <?php
+                error_reporting(0);
+                if(isset($_POST)){
+                    $nama = $_POST["nama"];
+                    $email = $_POST["email"];
+                    $pass = $_POST["pass"];
+                    $nomor = $_POST["nomor"];
+
+                    echo "
+                        <table>
+                            <tr>
+                                <td>Nama:</td>
+                                <td>$nama</td>
+                            </tr>
+                            <tr>
+                                <td>Email:</td>
+                                <td>$email</td>
+                            </tr>
+                            <tr>
+                                <td>Password:</td>
+                                <td>$pass</td>
+                            </tr>
+                            <tr>
+                                <td>Nomor:</td>
+                                <td>$nomor</td>
+                            </tr>
+                        </table>
+                    ";
+                }
+            
+            ?>
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+        </div>
+
+        
+    </div>
+
+    <div class="transition">
+        <div class="cover cover1"></div>
+        <div class="cover cover2"></div>
+        <div class="cover cover3"></div>
+    </div>
+
+    <div class="footer">
+        <footer class="textfooter">Copyright &copy;2022 <br>ivan maulana anggita </footer>
+    </div>
+
+    <script>
+        const link = document.querySelector(".link");
+        const transition = document.querySelector(".transition");
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            transition.classList.add("slide");
+            setTimeout(() => {
+                window.location = link.href;
+            }, 1200);
+        });
+    </script>
+    
+</body>
+</html>
